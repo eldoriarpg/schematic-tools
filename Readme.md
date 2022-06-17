2babd83028261f7e84940468982581e937cd5894
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2022-06-17 12:12:54 +0200

    Initial project setup

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Schematic Tools
+
+Schematic Tools is an Add-On for [Schematic Brush 2.0](https://github.com/eldoriarpg/SchematicBrushReborn).
+
+Bind schematics to tools and allow public usage.
---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+rootProject.name = "schematic-tools"
+
+pluginManagement{
+    repositories{
+        mavenLocal()
+        gradlePluginPortal()
+        maven{
+            name = "EldoNexus"
+            url = uri("https://eldonexus.de/repository/maven-public/")
+
+        }
+    }
+}
