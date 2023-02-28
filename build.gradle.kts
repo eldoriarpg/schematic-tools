5e9af9598927539475af2ada61495784bf033048
Florian Fülling <46890129+RainbowDashLabs@users.noreply.github.com>
2023-02-28 10:32:25 +0100

    Switch to spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
 import de.chojo.Repo
 
 plugins {
-    id("org.cadixdev.licenser") version "0.6.1"
+    id("com.diffplug.spotless") version "6.15.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
     id("de.chojo.publishdata") version "1.2.4"
     id("net.minecrell.plugin-yml.bukkit") version "0.5.3"
     java
@@ -26,9 +27,11 @@ dependencies {
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.2")
 }
 
-license {
-    header(rootProject.file("HEADER.txt"))
-    include("**/*.java")
+spotless{
+    java{
+        licenseHeaderFile(rootProject.file("HEADER.txt"))
+        target("**/*.java")
+    }
 }
 
 java {
